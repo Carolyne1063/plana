a912e7fae86bcc86916271d744d0cd66aae58dd4
Carolyne1063 <carolmartoz763@gmail.com>
2024-07-10 20:07:26 -0700

    added dummy text for the design

--- frontend/plana/src/app/components/event-manager/manager-home/manager-home.component.ts
+++ frontend/plana/src/app/components/event-manager/manager-home/manager-home.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-manager-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './manager-home.component.html',
   styleUrl: './manager-home.component.css'
 })
 export class ManagerHomeComponent {
+  showForm = false;
+  eventData = {
+    eventname: '',
+    price: '',
+    description: '',
+    location: '',
+    time: '',
+    date: ''
+  };
 
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+
+  createEvent() {
+    console.log('Event Created:', this.eventData);
+    // Implement event creation logic here
+    this.toggleForm();
+  } 
 }
