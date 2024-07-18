87c68898f3c512034a63b1975bb6b358d54cc7c3
Carolyne1063 <carolmartoz763@gmail.com>
2024-07-18 14:31:19 -0700

    integrated all events in user dashbaord

--- frontend/plana/src/app/components/user-dashboard/user-home/user-home.component.ts
+++ frontend/plana/src/app/components/user-dashboard/user-home/user-home.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../../services/eventService/event.service';
+import { Event } from '../../../interfaces/event';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './user-home.component.html',
   styleUrl: './user-home.component.css'
 })
-export class UserHomeComponent {
+export class UserHomeComponent implements OnInit {
+  events: Event[] = [];
+
+  constructor(private eventService: EventService) {}
+
+  ngOnInit() {
+    this.getEvents();
+  }
+
+  getEvents() {
+    this.eventService.getAllEvents().subscribe(
+      (data: Event[]) => {
+        this.events = data;
+      },
+      (error) => {
+        console.error('Error fetching events:', error);
+      }
+    );
+  }
+
   openForm() {
     const form = document.getElementById("bookingForm");
     if (form) form.style.display = "block";
